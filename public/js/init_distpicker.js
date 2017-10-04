3762a8603b78c7422c0c481d272f0a36bca402de
hldh214 <hldh214@gmail.com>
2017-10-04 16:53:03 +0800

    finish admin 4 Order CURD

--- public/js/init_distpicker.js
+++ public/js/init_distpicker.js
@@ -1,14 +1,22 @@
-$('.province').each(function (_, value) {
-    let province_no = $(value).data('no');
-    $(value).text($(document).distpicker('getDistricts')[province_no]);
-});
+function init_distpicker() {
+    $('.province').each(function (_, value) {
+        let province_no = $(value).data('no');
+        $(value).text($(document).distpicker('getDistricts')[province_no]);
+    });
 
-$('.city').each(function (_, value) {
-    let city_no = $(value).data('no');
-    $(value).text($(document).distpicker('getDistricts', $(value).prev().data('no'))[city_no]);
-});
+    $('.city').each(function (_, value) {
+        let city_no = $(value).data('no');
+        $(value).text($(document).distpicker('getDistricts', $(value).prev().data('no'))[city_no]);
+    });
 
-$('.district').each(function (_, value) {
-    let district_no = $(value).data('no');
-    $(value).text($(document).distpicker('getDistricts', $(value).prev().data('no'))[district_no]);
+    $('.district').each(function (_, value) {
+        let district_no = $(value).data('no');
+        $(value).text($(document).distpicker('getDistricts', $(value).prev().data('no'))[district_no]);
+    });
+}
+
+init_distpicker();
+
+$('#pjax-container').on('pjax:end', function () {
+    init_distpicker();
 });
